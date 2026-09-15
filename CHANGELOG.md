76d510f106c5ebeaf2472e0fd8b92061cd4321c0
YoriKv <yori.kvitchko@gmail.com>
2026-09-15 09:39:55 -0700

    Release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## v0.1.2 - unreleased
+## v0.1.2 - 2026-09-15
 
 - Code and tooltip fixes
 
--- src/mapchar/__init__.py
+++ src/mapchar/__init__.py
@@ -1,6 +1,6 @@
 """mapchar: a text viewer and editor for retro-game ROMs."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 #: The name the user sees: window titles, dialogs, the About box.
 APP_NAME = "mapChar"
